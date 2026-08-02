6e79b082d82af94fe78169566c4e9d285bc97703
madhumitamohanty <madhumitamohanty007@gmail.com>
2026-08-02 19:41:06 +0530

    Add files via upload

---
+++ AreaOfRectangle.java
@@ -0,0 +1,10 @@
+public class AreaOfRectangle {
+public static void main(String[] args) {
+int length=Integer.parseInt(args[0]);
+int breadth=Integer.parseInt(args[1]);
+int area=length*breadth;
+System.out.println("Length="+length);
+System.out.println("Breadth="+breadth);
+System.out.println("Area of Rectangle="+area);
+}
+}
